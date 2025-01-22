1d4771aba0e9bb489a1e3798ac9406cca54501aa
“xgui17” <xgui@ucsd.edu>
2025-01-21 22:11:25 -0800

    modified data cleaning step

--- sample_data.py
+++ sample_data.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import random
 
-data = pd.read_csv('9606.protein.links.v12.0.txt', sep='\t', names=['link_info'])
+# Note that this dataset is too big to be put on GitHub. You can find the file at https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz 
+data = pd.read_csv('../9606.protein.links.v12.0.txt', sep='\t', names=['link_info'])
 data = data.loc[1:]
 data[['protein1', 'protein2', 'combined_score']] = data['link_info'].str.split(expand=True)
 data.drop(columns=['link_info'], inplace=True)
 data['combined_score'] = data['combined_score'].astype(int)
-data['protein1'] = data['protein1'].apply(lambda x: x.replace("9606.ENSP000",""))
-data['protein2'] = data['protein2'].apply(lambda x: x.replace("9606.ENSP000",""))
-data.to_csv("cleaned_PPI.csv")
+data['protein1'] = data['protein1'].apply(lambda x: x.replace("9606.",""))
+data['protein2'] = data['protein2'].apply(lambda x: x.replace("9606.",""))
+data.to_csv("../cleaned_PPI_full_name.csv")
 
 random.seed(42)
 
--- src_customized/main.py
+++
@@ -1,9 +0,0 @@
-from Input import load_data
-from trainGCN import train_baseline_gae
-
-# Load data
-adj_matrix, protein_to_idx = load_data('/home/xgui/private/Q2_GAE/filtered_PPI.csv')
-print("Loaded data!")
-
-# Train baseline GAE
-train_baseline_gae(adj_matrix, latent_dim=16, num_epochs=10, lr=0.01)
